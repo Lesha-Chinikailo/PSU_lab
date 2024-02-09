6802221d1fe610c2e28eccba27f95a6b70f78ae8
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2024-02-09 12:14:59 +0300

    add 1 lab PMS

---
+++ 6_semester/PMS/lab_1/app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.lab_1
+
+import android.app.Activity;
+import android.widget.Toast;
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab_1.ui.theme.Lab_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_LONG).show();
+        setContent {
+            Lab_1Theme {
+                setContentView(R.layout.activity_main);
+                // A surface container using the 'background' color from the theme
+//                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+//                    Greeting("Android")
+//
+//                }
+            }
+        }
+    }
+    @Override
+    protected override fun onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
+
+    }
+    @Override
+    protected override fun onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart()", Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected override fun onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_LONG).show();
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab_1Theme {
+        Greeting("Android")
+    }
+}
