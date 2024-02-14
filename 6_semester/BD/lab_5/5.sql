5860dab8fa88e05b6a995399c9dc926307c26892
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2024-02-14 15:17:45 +0300

    git add 5

---
+++ 6_semester/BD/lab_5/5.sql
@@ -0,0 +1,56 @@
+--NON UPDATABLE
+CREATE VIEW WorkersSpecialitiesView AS 
+SELECT 
+Worker.Name AS WorkerName,
+Speciality.NameSpeciality AS SpecialityName
+FROM Worker
+JOIN Speciality ON Worker.IdSpeciality = Speciality.IdSpeciality
+
+SELECT * FROM WorkersSpecialitiesView
+
+CREATE VIEW CompanyDirectorsView AS
+SELECT
+Worker.Name AS WorkerName,
+Brigade.TypeBrigade AS BrigadeType
+FROM Worker
+JOIN Brigade ON Worker.IdBrigade = Brigade.IdBrigade
+
+SELECT * FROM CompanyDirectorsView
+
+CREATE VIEW ShowWorkerInBrigadeAndDepartmentView AS
+SELECT Name, Speciality.NameSpeciality, Brigade.TypeBrigade
+	FROM Worker
+	JOIN Speciality ON Worker.IdSpeciality=Speciality.IdSpeciality
+	JOIN Brigade ON Worker.IdBrigade=Brigade.IdBrigade
+
+SELECT * FROM ShowWorkerInBrigadeAndDepartmentView
+
+--UPDATABLE
+
+CREATE VIEW AllInformationWorkerView AS
+SELECT Worker.Name, Worker.Surname, Worker.Email, Worker.Rate, Worker.IdSpeciality, Worker.IdBrigade
+FROM Worker
+
+INSERT INTO AllInformationWorkerView VALUES
+('Katya', 'Surname', 'kat123@mail.ru', 1, 4, 4)
+
+CREATE VIEW FullNameView AS
+SELECT Name, Surname
+FROM Worker
+
+SELECT * FROM FullNameView
+
+
+DROP VIEW FullNameView
+
+ALTER VIEW FullNameView AS
+SELECT Name, Surname, Email
+FROM Worker
+
+INSERT INTO AllInformationWorkerView VALUES
+('Katya', 'Surname', 'kat123@mail.ru', 1, 5, 5)
+
+SELECT * FROM Worker
+
+DELETE FROM AllInformationWorkerView
+WHERE IdBrigade = 5
