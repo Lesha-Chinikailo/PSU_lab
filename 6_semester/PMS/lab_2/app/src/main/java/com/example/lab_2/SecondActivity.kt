43dfdc34e1b2b1a32445173bacceda6764769398
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2024-03-30 21:31:14 +0300

    add 2 lab PMS

--- 6_semester/PMS/lab_2/app/src/main/java/com/example/lab_2/SecondActivity.kt
+++ 6_semester/PMS/lab_2/app/src/main/java/com/example/lab_2/SecondActivity.kt
@@ -1,11 +1,23 @@
 package com.example.lab_2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val button1 = findViewById<Button>(R.id.ok)
+        button1.setOnClickListener{
+            Toast.makeText(this, "you click on button Ok", Toast.LENGTH_LONG).show();
+        }
+        val button2 = findViewById<Button>(R.id.cancel)
+        button2.setOnClickListener{
+            Toast.makeText(this, "you click on button cancel", Toast.LENGTH_LONG).show();
+        }
     }
 }
--- 6_semester/PMS/lab_2/app/src/main/java/com/example/lab_2/TabWidgetActivity.kt
+++ 6_semester/PMS/lab_2/app/src/main/java/com/example/lab_2/TabWidgetActivity.kt
@@ -10,8 +10,26 @@ class TabWidgetActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tab_widget)
+        val tabHost = findViewById<TabHost>(android.R.id.tabhost)
+        tabHost.setup()
 
-        setupTabs()
+        val inflater = LayoutInflater.from(this)
+
+        val tab1View = inflater.inflate(R.layout.tab1_content, null)
+        val tab2View = inflater.inflate(R.layout.tab2_content, null)
+
+        val tab1 = tabHost.newTabSpec("Tab 1")
+        tab1.setIndicator("Tab 1")
+        tab1.setContent { tab1View }
+        tabHost.addTab(tab1)
+
+        val tab2 = tabHost.newTabSpec("Tab 2")
+        tab2.setIndicator("Tab 2")
+        tab2.setContent { tab2View }
+        tabHost.addTab(tab2)
+
+        tabHost.currentTab = 0
+        //setupTabs()
     }
 
     private fun setupTabs() {
